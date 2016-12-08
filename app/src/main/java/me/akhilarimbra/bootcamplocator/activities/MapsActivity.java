d066517be57cd39964eede88ff979607802101c5
Akhil Raj <akhilraj@Akhils-MacBook-Air.local>
2016-12-08 14:18:34 +0530

    added MainFragment

---
+++ app/src/main/java/me/akhilarimbra/bootcamplocator/activities/MapsActivity.java
@@ -0,0 +1,24 @@
+package me.akhilarimbra.bootcamplocator.activities;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import me.akhilarimbra.bootcamplocator.R;
+
+public class MapsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+    }
+
+}
